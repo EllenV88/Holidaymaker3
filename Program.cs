3590c44f079132e3a09c207ea2d73e8ddd8894be
AreiaAruanda <144889202+AreiaAruanda@users.noreply.github.com>
2023-12-11 11:51:31 +0100

    added additions

--- Program.cs
+++ Program.cs
@@ -11,12 +11,30 @@ address TEXT,
 city TEXT, 
 country TEXT, 
 beach_distance NUMERIC, 
-center_distaance NUMERIC, 
+center_distance NUMERIC, 
 rating NUMERIC, 
 restaurant BOOLEAN, 
 pool BOOLEAN, 
 evening_entertainment BOOLEAN, 
-kids_club BOOlEAN
+kids_club BOOLEAN,
+price_extrabeds MONEY, 
+price_halfboard_child MONEY, 
+price_halfboard_adult MONEY, 
+price_allinclusive_child MONEY, 
+price_allinclusive_adult MONEY
+)"))
+
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+await using (var cmd = db.CreateCommand(@"CREATE TABLE IF NOT EXISTS additions(
+id SERIAL PRIMARY KEY, 
+number_of_extrabeds INT DEFAULT NULL CHECK (number_of_extrabeds <= 1), 
+number_of_halfboard_child INT DEFAULT NULL, 
+number_of_halfboard_adult INT DEFAULT NULL, 
+number_of_allinclusive_child INT DEFAULT NULL, 
+number_of_allinclusive_adult INT DEFAULT NULL
 )"))
 
 {

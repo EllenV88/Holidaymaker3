43d27b8f8660f8af8a15f86adce67a224039a352
FredrikH03 <fredrikholst03@hotmail.com>
2023-12-12 09:32:51 +0100

    Moved creating database, creating tables and populating to same file
    Should the menu also be moved to the same file as to reduce clutter on
    program.cs?

--- Program.cs
+++ Program.cs
@@ -1,60 +1,22 @@
 ﻿using Npgsql;
+using makedatabase;
+using System.Runtime.CompilerServices;
 
-
-/*
-string dbUri2 = "Host=localhost;Port=5455;Username=postgres;Password=postgres;";
-        
-await using var db2 = NpgsqlDataSource.Create(dbUri2);
-
-await using (var cmd = db2.CreateCommand("CREATE DATABASE holidaymaker"))
-{ 
-    await cmd.ExecuteNonQueryAsync();
+Console.WriteLine("do you want to create the database?");
+string userinput = Console.ReadLine();
+if ("y" == userinput || "" == userinput){
+    Console.WriteLine("testtest");
+    await Script.CreateDatabase();
 }
-*/
-
-string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=holidaymaker;";
-
-await using var db = NpgsqlDataSource.Create(dbUri);
-
-await using (var cmd = db.CreateCommand(
-                 "CREATE TABLE IF NOT EXISTS customers (id SERIAL PRIMARY KEY, first_name TEXT, last_name TEXT, email TEXT, number INTEGER, date_of_birth DATE)"))
-{
-    await cmd.ExecuteNonQueryAsync();
+Console.WriteLine("do you want to create all the tables?");
+userinput = Console.ReadLine();
+if ("y" == userinput || "" == userinput){
+    Console.WriteLine("testtest");
+    await Script.MakeTables();
 }
-
-await using (var cmd = db.CreateCommand(@"CREATE TABLE IF NOT EXISTS hotels (
-id SERIAL PRIMARY KEY,
-name TEXT, 
-address TEXT, 
-city TEXT, 
-country TEXT, 
-beach_distance NUMERIC, 
-center_distance NUMERIC, 
-rating NUMERIC, 
-restaurant BOOLEAN, 
-pool BOOLEAN, 
-evening_entertainment BOOLEAN, 
-kids_club BOOLEAN,
-price_extrabeds MONEY, 
-price_halfboard_child MONEY, 
-price_halfboard_adult MONEY, 
-price_allinclusive_child MONEY, 
-price_allinclusive_adult MONEY
-)"))
-
-{
-    await cmd.ExecuteNonQueryAsync();
-}
-
-await using (var cmd = db.CreateCommand(@"CREATE TABLE IF NOT EXISTS additions(
-id SERIAL PRIMARY KEY, 
-number_of_extrabeds INT DEFAULT NULL CHECK (number_of_extrabeds <= 1), 
-number_of_halfboard_child INT DEFAULT NULL, 
-number_of_halfboard_adult INT DEFAULT NULL, 
-number_of_allinclusive_child INT DEFAULT NULL, 
-number_of_allinclusive_adult INT DEFAULT NULL
-)"))
-
-{
-    await cmd.ExecuteNonQueryAsync();
+Console.WriteLine("do you want to populate the database");
+userinput = Console.ReadLine();
+if ("y" == userinput || "" == userinput){
+    Console.WriteLine("testtest");
+    //await Script.notimplemented();
 }
--- makedatabase.cs
+++ makedatabase.cs
@@ -1,13 +1,10 @@
 using Npgsql;
 
 namespace makedatabase;
-class script 
+class Script 
 {
-    static async Task createdatabase()
-    { 
-        //OBS, MÅSTE GÖRA EN .CSPROJ FÖR ATT KÖRA PROGRAMMET, OCH DU MÅSTA LÄGGA TILL NPGSQL OCKSÅ
-        //gör det såhär då detta inte kommer vara huvudprogrammet, men jag vill skjuta upp det ändå
-            
+    
+    public static async Task CreateDatabase(){
         string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;";
         
         await using var db = NpgsqlDataSource.Create(dbUri);
@@ -16,23 +13,54 @@ class script
         { 
             await cmd.ExecuteNonQueryAsync();
         }
-                
-            
-            /*
-            bool addtables = true;
-            while (addtables = true)
-            {
-                string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=holidaymaker;";
-
-                await using var db = NpgsqlDataSource.Create(dbUri);
-
-
-                //ska telefonnumret vara TEXT eller INTEGER?
-                await using (var cmd = db.CreateCommand(
-                                 "CREATE TABLE IF NOT EXISTS customer (id SERIAL PRIMARY KEY, first_name TEXT, last_name TEXT, email TEXT, number INTEGER, date_of_birth DATE)"))
-                {
-                    await cmd.ExecuteNonQueryAsync();
-                }
-            }*/
-        }
     }
+    public static async Task MakeTables()
+    { 
+        string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=holidaymaker;";
+
+        await using var db = NpgsqlDataSource.Create(dbUri);
+
+        await using (var cmd = db.CreateCommand(
+                            "CREATE TABLE IF NOT EXISTS customers (id SERIAL PRIMARY KEY, first_name TEXT, last_name TEXT, email TEXT, number INTEGER, date_of_birth DATE)"))
+        {
+           await cmd.ExecuteNonQueryAsync();
+        }
+
+        await using (var cmd = db.CreateCommand(@"CREATE TABLE IF NOT EXISTS hotels (
+            id SERIAL PRIMARY KEY,
+            name TEXT, 
+            address TEXT, 
+            city TEXT, 
+            country TEXT, 
+            beach_distance NUMERIC, 
+            center_distance NUMERIC, 
+            rating NUMERIC, 
+            restaurant BOOLEAN, 
+            pool BOOLEAN, 
+            evening_entertainment BOOLEAN, 
+            kids_club BOOLEAN,
+            price_extrabeds MONEY, 
+            price_halfboard_child MONEY, 
+            price_halfboard_adult MONEY, 
+            price_allinclusive_child MONEY, 
+            price_allinclusive_adult MONEY
+        )"))
+
+        {
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        await using (var cmd = db.CreateCommand(@"CREATE TABLE IF NOT EXISTS additions(
+            id SERIAL PRIMARY KEY, 
+            number_of_extrabeds INT DEFAULT NULL CHECK (number_of_extrabeds <= 1), 
+            number_of_halfboard_child INT DEFAULT NULL, 
+            number_of_halfboard_adult INT DEFAULT NULL, 
+            number_of_allinclusive_child INT DEFAULT NULL, 
+            number_of_allinclusive_adult INT DEFAULT NULL
+            )"))
+
+        {
+            await cmd.ExecuteNonQueryAsync();
+        }
+    }    
+}

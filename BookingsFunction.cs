36ae769b00130edbd72ff8269c97787c1b43c0ab
FredrikH03 <fredrikholst03@hotmail.com>
2023-12-20 16:03:37 +0100

    removed extra Console.ReadLine that was added for some reason

--- BookingsFunction.cs
+++ BookingsFunction.cs
@@ -52,7 +52,6 @@ public class BookingFunction
             Console.WriteLine("4 - Suite");
             Console.Write("\nPlease enter the desired room type: ");
             bool success = int.TryParse(Console.ReadLine(), out roomID);
-            string input = Console.ReadLine();
             if (true == success){
                 insertLoop = false;
             }
@@ -83,7 +82,7 @@ public class BookingFunction
         while(true == insertLoop){ //adults
             Console.Write("\nPlease enter the number of adults: ");
             bool success = int.TryParse(Console.ReadLine(), out numberOfAdults);
-            if (true == succes)
+            if (true == success)
             {
                 insertLoop = false;
             }
@@ -114,12 +113,12 @@ public class BookingFunction
             await cmd.ExecuteNonQueryAsync();
         }
         
-        Console.WriteLine(@"what extras do you want
-                            1, Extra bed
-                            2, Half board child
-                            3, Half board adult
-                            4, All-inclusive child
-                            5, All-inclusive adult");
+        Console.WriteLine("what extras do you want" +
+                            "1, Extra bed" +
+                            "2, Half board child" +
+                            "3, Half board adult" +
+                            "4, All-inclusive child" +
+                            "5, All-inclusive adult");
 
         string chosenExtrasString = Console.ReadLine();
         int booking = 0;
